433c1c1e2b4d3f55d0b3c354a9a47a656ff3b892
Markus Emrich <Markus.Emrich@gmail.com>
2014-07-08 18:52:36 +0200

    ported rotation methods, fixed some issues

--- JDStatusBarNotification_Swift/StatusBarNotification.swift
+++ JDStatusBarNotification_Swift/StatusBarNotification.swift
@@ -18,6 +18,9 @@ class StatusBarNotification {
     var defaultStyle:StatusBarStyle
     var userStyles:Dictionary<String,StatusBarStyle>
     
+    var overlayWindow:UIWindow?
+    var progressView:UIView?
+    var topBar:StatusBarView?
     
     // ---------------------------------- Singleton ------------------------------------------ //
     
@@ -119,7 +122,7 @@ class StatusBarNotification {
     }
     
     // dismissing
-    func setDismissTimer(interval:NSTimeInterval)
+    func setDismissTimer(#interval:NSTimeInterval)
     {
         let fireDate = NSDate(timeIntervalSinceNow:interval)
         
@@ -137,45 +140,31 @@ class StatusBarNotification {
     {
         dismissTimer?.invalidate()
         dismissTimer = nil
+        
+        if (!self.overlayWindow) { return }
+        if (!self.topBar) { return }
     
         // check animation type
-        let animationsEnabled = (activeStyle.animationType != .None)
-        animated = animated && animationsEnabled
+        let animationsEnabled = (activeStyle.animationType != StatusBarStyle.AnimationType.None)
+        let shouldAnimate = animated && animationsEnabled
 
-//        // animate out
-//        [UIView animateWithDuration:animated ? 0.4 : 0.0 animations:^{
-//        if (self.activeStyle.animationType == JDStatusBarAnimationTypeFade) {
-//        self.topBar.alpha = 0.0;
-//        } else {
-//        self.topBar.transform = CGAffineTransformMakeTranslation(0, -self.topBar.frame.size.height);
-//        }
-//        } completion:^(BOOL finished) {
-//        [self.overlayWindow removeFromSuperview];
-//        [self.overlayWindow setHidden:YES];
-//        _overlayWindow.rootViewController = nil;
-//        _overlayWindow = nil;
-//        _progressView = nil;
-//        _topBar = nil;
-//        }];
-    }
-    
-    // getter
-    var overlayWindow:UIWindow {
-    get {
-        
-    }
-    }
-    
-    var progressView:UIView {
-    get {
-        return UIView(frame:CGRectZero)
-    }
-    }
-    
-    var topBar:StatusBarView {
-    get {
-        
-    }
+        // animate out
+        let topBar:UIView = self.topBar!
+        let overlayWindow:UIWindow = self.overlayWindow!
+        UIView.animateWithDuration(shouldAnimate ? 0.4 : 0.0, animations: {
+            if (self.activeStyle.animationType == StatusBarStyle.AnimationType.Fade) {
+                topBar.alpha = 0.0
+            } else {
+                topBar.transform = CGAffineTransformMakeTranslation(0, -topBar.frame.size.height)
+            }
+            }, completion: {(completed) in
+                self.overlayWindow!.removeFromSuperview()
+                self.overlayWindow!.hidden = true
+                self.overlayWindow!.rootViewController = nil
+                self.overlayWindow = nil
+                self.progressView = nil
+                self.topBar = nil
+            })
     }
     
     func addStyleNamed(identifier:String, prepare:(style:StatusBarStyle) -> StatusBarStyle) -> String
@@ -184,12 +173,41 @@ class StatusBarNotification {
         return identifier;
     }
     
+    // Rotation
+    func updateWindowTransform()
+    {
+        if (!overlayWindow) { return }
+        
+        var window = UIApplication.sharedApplication().keyWindow
+        if (window == nil && UIApplication.sharedApplication().windows.count > 0) {
+            window = UIApplication.sharedApplication().windows[0] as UIWindow
+        }
+        overlayWindow!.transform = window.transform
+        overlayWindow!.frame = window.frame
+    }
+    
+    func updateTopBarFrameWithStatusBarFrame(rect:CGRect)
+    {
+        if (!topBar) { return }
+        
+        let width:CGFloat  = max(rect.size.width, rect.size.height)
+        let height:CGFloat = min(rect.size.width, rect.size.height)
+        
+        // on ios7 fix position, if statusBar has double height
+        var yPos:CGFloat = 0;
+        if (UIDevice.currentDevice().systemVersion.bridgeToObjectiveC().floatValue >= 7.0 && height > 20.0) {
+            yPos = -height/2.0;
+        }
+        
+        topBar!.frame = CGRectMake(0, yPos, width, height);
+    }
+    
     func willChangeStatusBarFrame(notification:NSNotification)
     {
         let barFrameValue:NSValue = notification.userInfo[UIApplicationStatusBarFrameUserInfoKey] as NSValue
         UIView.animateWithDuration(0.5) {
-            updateWindowTransform()
-            updateTopBarFrameWithStatusBarFrame(barFrameValue.CGRectValue)
+            self.updateWindowTransform()
+            self.updateTopBarFrameWithStatusBarFrame(barFrameValue.CGRectValue())
             
             // update progress with current value
             let progress = self.progress;
